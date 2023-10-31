4158b633a38d8ef4d26b36b5df8955f0575543b7
Daniel Shiffman <daniel.shiffman@gmail.com>
2023-10-31 13:57:53 -0400

    absolute path

--- functions/serve-image.js
+++ functions/serve-image.js
@@ -1,14 +1,22 @@
 const fs = require('fs');
 const path = require('path');
 
-exports.handler = async (event) => {
-  const filePath = path.join(__dirname, 'a3f5c11b2/meowmeow-square.png');
-  const imageBuffer = fs.readFileSync(filePath);
-
-  return {
-    statusCode: 200,
-    headers: { 'Content-Type': 'image/png' },
-    body: imageBuffer.toString('base64'),
-    isBase64Encoded: true,
-  };
+exports.handler = async (event, context) => {
+  try {
+    const imagePath = path.join(__dirname, 'meowmeow-square.png');
+    const imageBuffer = fs.readFileSync(imagePath);
+    return {
+      statusCode: 200,
+      body: imageBuffer.toString('base64'),
+      headers: {
+        'Content-Type': 'image/png',
+      },
+      isBase64Encoded: true,
+    };
+  } catch (error) {
+    return {
+      statusCode: error.statusCode || 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
 };
--- sketch.js
+++ sketch.js
@@ -24,7 +24,7 @@ class Tile {
 }
 
 function preload() {
-  source = loadImage('meowmeow-square.png');
+  source = loadImage('a3f5c11b2/meowmeow-square.png');
 }
 
 function setup() {
